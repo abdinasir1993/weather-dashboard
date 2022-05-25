79a3075ce2ca2bebea81f41b5aded7518abb1456
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-05-25 17:40:09 +0100

    rendered current weather card

--- index.js
+++ index.js
@@ -21,20 +21,72 @@ const renderWeatherData = () => {
   // use API to fetch current weather data
   let search = document.getElementById("input-text").value;
   const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}`;
+  console.log(currentWeatherUrl)
   
-  console.log (currentWeatherUrl)
+  let items = [];
+
+  fetch(currentWeatherUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (result) {
+      items = result;
+      console.log(items)
+    
+      let cityName = items.name;
+      console.log (cityName)
+      let date =  items.dt;
+      console.log(date)
+      
+      let temperature = items.main.temp;
+      console.log(temperature)
+
+      let humidity =  items.main.humidity;
+
+      console.log (humidity)
+
+      let windSpeed = items.wind.speed;
+      console.log(windSpeed)
+
+      $("#card-box").append(`<div><h1>Current Weather Data</h1> </div>
+
+        
+      <div>
+       
+        <h2>Location:${cityName} </h2>
+        <div class=" d-flex justify-content-between flex-wrap">
+          
+          <div class="card" style="width: 12rem">
+            <img src="" class="card-img-top" alt="" />
+            <div class="card-body">
+              
+              <h5 class="card-title">Temperature:${temperature} </h5>
+              <h5 class="card-title">Humidity:${humidity} </h5>
+              <h5 class="card-title">Windspeed:${windSpeed} </h5>
+              <p class="card-text">
+             
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+`)
 
 
-  // from the response cherry pick all the data you want to see in the current weather card
+      
+        
+    })
+    
+    
+};
+
 
   // get the lat and lon from current weather data API response
 //   const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}`;
 
   // render current weather data
 
-  // render forecast weather data
-};
-
+  // render forecast weather dat
 const handleFormSubmit = (event) => {
     
     event.preventDefault();
@@ -55,8 +107,8 @@ const handleFormSubmit = (event) => {
 
 const onReady = () => {
   // render recent cities
+  console.log('ready')
 };
 form.addEventListener("submit", handleFormSubmit);
 
 $(document).ready(onReady);
-
