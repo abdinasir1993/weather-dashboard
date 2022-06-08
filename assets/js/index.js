e36e48cb05fee4acd1c9d210c99ed5f1ab48e1d7
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-06-08 16:33:09 +0100

    started the review

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,16 @@
 const API_KEY = "d92eced4f070a72612c2186a9ea527d8";
 
+
 // const renderCities = () => {
 //   // get recent cities from LS []u
 //   // if [] is empty then render alert
 //   // else render all recent cities
 //   // add an event listener on div containing all cities
 // };
-
-// const renderCurrentWeather = (currentWeatherData) => {
-//   // render the current weather data and append to section
-// };
-
-// const renderForecastWeather = (forecastWeatherData) => {
-//   // render the forecast weather data and append each card to section
-// };
+//render weather forecast
 const renderWeatherForecast = (lat, lon) => {
   const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}`
-  console.log(forecastWeatherUrl)
+  
   let items = [];
   
 
@@ -26,20 +20,22 @@ const renderWeatherForecast = (lat, lon) => {
     })
     .then(function (result) {
       items = result;
-      console.log(items)
+     
+      // forloop for 5 day forecast
     
   for (i = 0; i < 5; i++){
 
     let temp = items.daily[i].temp.day
-    console.log (temp)
     
     let icon = items.daily[i].weather[0].icon
-    console.log (icon)
+    
     let tempDate = items.daily[i].dt
     let date = moment.unix(tempDate).format('DD-MM-YYYY');
-    console.log(date)
+   
     let description = items.daily[i].weather[0].description
     
+  //  appended forecast
+  
     $("#forecast").append(
       ` <div>
       <!-- title -->
@@ -49,11 +45,11 @@ const renderWeatherForecast = (lat, lon) => {
 
     
         <div class="card" style="width: 12rem">
-        <h5 class="card-title">${date}</h5>
+        <h5>${date}</h5>
           <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="card-img-top" alt="..." />
           <div class="card-body">
-          <h5 class="card-title">${temp}℃</h5>
-            <p class="card-text">
+          <h5 >${temp}℃</h5>
+            <p >
             ${description}
             </p>
             
@@ -66,12 +62,11 @@ const renderWeatherForecast = (lat, lon) => {
 
 
 
-
-const renderWeatherData = () => {
+// render current weather
+const renderCurrentWeather = (search) => {
   // use API to fetch current weather data
-  let search = document.getElementById("input-text").value;
   const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}&units=metric`;
-  console.log(currentWeatherUrl)
+  
   
   let items = [];
   
@@ -82,58 +77,44 @@ const renderWeatherData = () => {
     })
     .then(function (result) {
       items = result;
-      console.log(items)
+      
     
       let cityName = items.name;
-      console.log (cityName)
+      
       
      let  tempDate = items.dt;
      let  date = moment.unix(tempDate).format('DD-MM-YYYY');
-      console.log(date)
+      
       
       let temperature = items.main.temp;
-      console.log(temperature)
+    
 
       let humidity =  items.main.humidity;
 
-      console.log (humidity)
 
       let windSpeed = items.wind.speed;
     
       
       let icon = items.weather[0].icon
-      console.log(icon)
-
+      
       let visibility = items.visibility
 
       let pressure = items.main.pressure
 
       let clouds = items.weather[0].description
+      
+      let feelslike = items.main.feels_like
 
       let cloudiness = ""
       
       
 
-      
-       
-      // if (clouds < 20 )  {
-      //  cloudiness = "clear sky"
-
-      // } else if (clouds > 20 && clouds < 40 ) {
-      //   cloudiness = "light cloud"
-
-      // } else if (clouds >41 && clouds < 60 ) {
-      //   cloudiness = "cloudy"
-
-      // } else   {
-      //   cloudiness = "mostly cloudy"
-
-      // }
-      console.log (cloudiness)
-
-      // if (icon = 01d.png ) 
+ 
       const main = document.getElementById("main")
+      
       main.setAttribute('class', `a${icon}`)
+
+      // rendered date function
 
   
       const renderDate = () => {
@@ -148,11 +129,13 @@ const renderWeatherData = () => {
 
       
 
-      let feelslike = items.main.feels_like
+      // rendering current weather section
+
+
 
       $("#all-container").append(` <h2> </h2>
       <section class="current-weather-section">
-        <div class="main-weather-card">
+        <div >
             
           <div class="information">
               <div>
@@ -163,12 +146,12 @@ const renderWeatherData = () => {
           </div>
           
           <div class="information1">
-              <div class ="info1">
+              <div >
               <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">
             <h3>${temperature}℃</h3>
         </div>
         
-            <div class="info1">
+            <div >
             <h3>${clouds} </h3>
             <h4>feels like : ${feelslike}℃ </h4>
         </div>
@@ -216,24 +199,16 @@ const renderWeatherData = () => {
 
 
 
-
-  // get the lat and lon from current weather data API response
-  // const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}`;
-
-  // render current weather data
-
-  // render forecast weather dat
 const handleFormSubmit = (event) => {
 
   const container = document.getElementById("container")
-  const main = document.getElementById("main")
+  
   
   container.remove()
 
- 
-
-  $("#main").append(` <section id="container">
-  <section id="all-container" class=" section4 main-partrow border">
+  const mainContainer = document.getElementById("main-container")
+  $("#main-container").append(` <section id="container">
+  <section id="all-container" >
 
  </section>
 <!-- Form and recent history section -->
@@ -254,14 +229,13 @@ const handleFormSubmit = (event) => {
     event.preventDefault();
     //get text input
     let search = document.getElementById("input-text").value;
-    console.log(search);
-    //validate
-  renderWeatherData()
-  
+   
+   
 
     if (search) {
-      // build object with full name and results
-      console.log("good search");
+       //validate
+  renderCurrentWeather(search)
+  
       
     } else {
       alert("enter valid search");
@@ -270,8 +244,8 @@ const handleFormSubmit = (event) => {
   
 
 const onReady = () => {
-  // render recent cities
-  console.log('ready')
+  /
+  
 };
 const form1 = document.getElementById("form")
 form1.addEventListener("submit",handleFormSubmit);
